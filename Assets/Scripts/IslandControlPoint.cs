883d3cbec785a173968d0d993b8394804688dc48
JohnETrowbridge <jtrowbridge3@student.alamo.edu>
2021-09-27 14:50:00 -0500

    Island Movement and Control Point Updates
    Scripts for Island Movement, Control Point, and Camera Movement are updated.

--- Assets/Scripts/IslandControlPoint.cs
+++ Assets/Scripts/IslandControlPoint.cs
@@ -13,16 +13,9 @@ public class IslandControlPoint : MonoBehaviour
     public int CamMode;
     private bool triggeractive = false;
 
-    private void OnTriggerEnter(Collider other)
+    void Update()
     {
-        if (other.tag == "Player")
-        {
-            Debug.Log("Player is on Trigger.");
-        }
-    }
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.tag == "Player")
+        if (triggeractive == true)
         {
             if (Input.GetKeyDown("r"))
             {
@@ -30,17 +23,8 @@ public class IslandControlPoint : MonoBehaviour
                 Debug.Log("Player has activated Trigger.");
                 characterController.canMove = false;
                 IslandMovement.islandMove = 1;
-                if (CamMode == 1)
-                {
-                    CamMode = 0;
-                }
-                else
-                {
-                    CamMode += 1;
-                }
-                StartCoroutine(CamChange());
-                //CameraControl.MainCamera.SetActive(false);
-                //CameraControl.IslandCamera.SetActive(true);
+                ThirdCam.SetActive(false);
+                FirstCam.SetActive(true);
 
             }
             else if (Input.GetKeyDown("f") && triggeractive == true)
@@ -49,39 +33,27 @@ public class IslandControlPoint : MonoBehaviour
                 Debug.Log("Player has deactivated Trigger.");
                 characterController.canMove = true;
                 IslandMovement.islandMove = 0;
-                if (CamMode == 1)
-                {
-                    CamMode = 0;
-                }
-                else
-                {
-                    CamMode += 1;
-                }
-                //CameraControl.MainCamera.SetActive(true);
-                //CameraControl.IslandCamera.SetActive(false);
+                ThirdCam.SetActive(true);
+                FirstCam.SetActive(false);
             }
         }
-
-
-        //CameraControl.CameraSwitch();
-        //cameras.GetComponent<CameraControl>().CameraSwitch();
-        //CameraControl.cameras[0].gameObject.SetActive(false);
-        //CameraControl.cameras[1].gameObject.SetActive(true);
     }
 
-    IEnumerator CamChange()
+    private void OnTriggerEnter(Collider other)
     {
-        yield return new WaitForSeconds(0.01f);
-
-        if (CamMode == 0)
+        if (other.tag == "Player")
         {
-            ThirdCam.SetActive(true);
-            FirstCam.SetActive(false);
+            triggeractive = true;
+            Debug.Log("Player is on Trigger.");
         }
-        if (CamMode == 1)
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
         {
-            ThirdCam.SetActive(false);
-            FirstCam.SetActive(true);
+            triggeractive = false;
+            Debug.Log("Player is on Trigger.");
         }
     }
 }

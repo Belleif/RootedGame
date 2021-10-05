1160830e8849054acf1fa680e1fc0e7f3604f78f
Safronixx <jboland5@student.alamo.edu>
2021-10-04 19:56:13 -0500

    Made Testing Boss Area
    Just made an area to quickly start working on boss island code

---
+++ Assets/Scripts/BossMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+/* This script should handle boss' AI pathfinding
+ * Make it trigger after the player enters the bosses island / Make seperate script that handles the detection of player in boss island with collision detection
+ * Make basic following pattern for now unless the boss needs to be stationary */

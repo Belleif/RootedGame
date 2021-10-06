f618ab3f8378b13e30f7700038ac9bdef6d82555
Safronixx <jboland5@student.alamo.edu>
2021-10-06 03:28:14 -0500

    Boss AI laying preliminary code
    Just handling the groundwork for the code for handling boss detection and movement as well as animation setup

--- Assets/Scripts/BossMovement.cs
+++ Assets/Scripts/BossMovement.cs
@@ -1,21 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BossMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform player;
+    public float health;
+    public bool playerSeen;
+    public bool attackPlayer;
+    GameObject playerDetect = new GameObject();
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+
     void Start()
     {
-        
+        playerSeen = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!playerSeen && !attackPlayer) 
+            Idle();
+        if (playerSeen && !attackPlayer)
+            FollowPlayer();
+        if (playerSeen && attackPlayer)
+            AttackPlayer();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //Set Bool of boss sees player to true and use it in boss movement to make the boss face player
+            playerSeen = true;
+        }
+    }
+
+    private void Idle()
+    {
+        //Set Animation Controller in here for possible Idle stance
+    }
+
+
+
+    private void FollowPlayer()
+    {
+        //Once player is in range this method makes it so the boss faces towards them
+        if(playerSeen == true)
+        {
+            Debug.Log("Boss sees Player.");
+        }
+    }
+
+    private void AttackPlayer()
+    {
+        if (!alreadyAttacked)
+        {
+            //Boss Attack Code Here
+
+            Debug.Log("Boss Attacked");
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+    //Handles Boss Damage and Boss Defeat
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            //Input Animation, Cutscene, whatever signals that the boss has been defeated.
+
+            DestroyEnemy();
+        }
+    }
+
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
     }
 }
 /* This script should handle boss' AI pathfinding
- * Make it trigger after the player enters the bosses island / Make seperate script that handles the detection of player in boss island with collision detection
- * Make basic following pattern for now unless the boss needs to be stationary */
+ * Need to make sure to link boss damage and islands being used to damage the boss*/
+

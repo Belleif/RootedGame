3998b455fe50049340e4efcb4311b9f3cd197ad4
JohnETrowbridge <jtrowbridge3@student.alamo.edu>
2021-10-11 13:21:24 -0500

    Added Survey Form to Main Menu Quit Button

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -17,6 +17,7 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame()
     {
+        Application.OpenURL("https://forms.gle/hJ4uXLn4VRNBkhpD7");
         Debug.Log("Program Quitting...");
         Application.Quit();
     }

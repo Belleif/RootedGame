5968b9ae7e7450f7b12470dcdedb0b6f90c9a74a
JohnETrowbridge <jtrowbridge3@student.alamo.edu>
2021-10-29 09:35:09 -0500

    Working on Async.

---
+++ Assets/Scripts/SceneLoading.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class SceneLoading : MonoBehaviour
+{
+
+    public Image progressBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadAsyncOperation());
+    }
+
+
+    IEnumerator LoadAsyncOperation()
+    {
+        //take this scene and load it
+        //create an async operation
+        //progress bar fill = async operation progress
+        //when it's done, load the game--could also prompt user for key press
+        //or unlock a button
+
+        //google AsyncOperation for Unity
+        //Hard coded scene 2
+        AsyncOperation gameLevel = SceneManager.LoadSceneAsync(0);
+        //check the load
+        while (gameLevel.progress < 1)
+        {
+            //fill bar
+            progressBar.fillAmount = gameLevel.progress;
+            Debug.Log(gameLevel.progress);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}

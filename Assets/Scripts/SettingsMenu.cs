ecc517306ea2834ccde19739c2b98d2b59aca38e
Safronixx <jboland5@student.alamo.edu>
2021-11-01 10:26:19 -0500

    Updated settings code
    Should have working keys that save player settings but I can't test cause the build wont work. Also added sliders and dropdown methods that might need rework too but I dont know

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -21,11 +21,12 @@ public class SettingsMenu : MonoBehaviour
     //For In-Game Pause Menu ONLY!
     public GameObject pauseMenuUI;
     public GameObject settingsMenuUI;
-
     public AudioMixer audioMixer;
-
     public Dropdown resolutionDropdown;
-
+    public Slider volume;
+    public Dropdown qualityDropdown;
+    int currentResolutionIndex = 0;// Moved this one out of resolution selection
+    public int fullscreencheck;
     Resolution[] resolutions;
 
     void Start()
@@ -37,13 +38,13 @@ public class SettingsMenu : MonoBehaviour
 
         resolutions = Screen.resolutions;
         Debug.Log(resolutions.Length);
-        int currentResolutionIndex = 0;
+        
         for (int i =0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height + " : " + resolutions[i].refreshRate + " hz";
             options.Add(option);
             Debug.Log(option);
-
+            //int Current resolution index was moved from here
             if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height && Screen.currentResolution.refreshRate == resolutions[i].refreshRate)
             {
                 currentResolutionIndex = i;
@@ -60,21 +61,55 @@ public class SettingsMenu : MonoBehaviour
     {
         Resolution resolution = resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        PlayerPrefs.SetInt("ResolutionPreference", resolutionDropdown.value);
     }
 
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("volume", volume);
+        PlayerPrefs.SetFloat("VolumePreference", volume);
     }
 
     public void SetQuality (int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+        PlayerPrefs.SetInt("QualityPreference", qualityIndex);
     }
 
     public void SetFullscreen (bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+        //Should convert true and false to int values
+        if(isFullscreen)
+        {
+            fullscreencheck = 1;
+        }
+        else
+        fullscreencheck = 0;
+        PlayerPrefs.SetInt("FullscreenPreference", fullscreencheck);
+    }
+
+    public void LoadSettings()
+    {
+        if (PlayerPrefs.HasKey("ResolutionPreference"))
+            resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionPreference");
+        else
+            resolutionDropdown.value = currentResolutionIndex;
+
+        if (PlayerPrefs.HasKey("VolumePreference"))
+           volume.value = PlayerPrefs.GetFloat("VolumePreference");
+        else
+            volume.value = 1.0f;
+
+        if (PlayerPrefs.HasKey("QualityPreference"))
+            qualityDropdown.value = PlayerPrefs.GetInt("QualityPreference");
+        else
+            qualityDropdown.value = 5;
+
+        if (PlayerPrefs.HasKey("FullscreenPreference"))
+            fullscreencheck = PlayerPrefs.GetInt("QualityPreference");
+        else
+            fullscreencheck = 0;
     }
 
     //For In-Game Pause Menu ONLY!

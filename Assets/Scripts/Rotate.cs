c8194e2dca638d678dba73c814a9f0ce0609e9fa
Justus Nicolas <nulgath16@gmail.com>
2021-11-17 22:25:38 -0600

    Added Rotate script to collects (Previous Collects I placed were gone)

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+  
+    void Update() {
+		transform.Rotate(new Vector3(0f, 0f, 50f)* Time.deltaTime);      
+    }
+}
+
+

ab6750f85bb7122c8009eac6aa2bb8dd4fe88779
JohnETrowbridge <jtrowbridge3@student.alamo.edu>
2021-11-29 21:12:53 -0600

    Fixing the Cursor Appearing in the Last Cutscene Level.

---
+++ Assets/Scripts/CursorAppear.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAppear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

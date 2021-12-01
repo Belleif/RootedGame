149d734b6b86a240f098a4e657901b4f112a4b3c
Justus Nicolas <nulgath16@gmail.com>
2021-11-30 22:55:11 -0600

    Collectable script redux
    Changed collectable scripts

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotate : MonoBehaviour {
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] float speedX;
+    [SerializeField] float speedY;
+	[SerializeField] float speedZ;
+	
 
-  
-    void Update() {
-		transform.Rotate(new Vector3(0f, 0f, 50f)* Time.deltaTime);      
+    void Update()
+    {
+        transform.Rotate(360 * speedX * Time.deltaTime, 360 * speedY * Time.deltaTime, 360 * speedZ * Time.deltaTime);
     }
 }
-
-

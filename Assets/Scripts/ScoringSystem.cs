66a0d452e8c7d7c5432161cc55aefa0f0c832509
Justus Nicolas <nulgath16@gmail.com>
2021-12-06 15:10:26 -0600

    Collect Score
    Colliding with a collect adds score will be tuned further.

--- Assets/Scripts/ScoringSystem.cs
+++ Assets/Scripts/ScoringSystem.cs
@@ -6,12 +6,13 @@ using UnityEngine.UI;
 public class ScoringSystem : MonoBehaviour {
 
     public GameObject scoreText;
-	public int theScore;
+	public static int theScore;
 	public AudioSource CollectSound;
 	
 	void OnTriggerEnter(Collider other)
 	{
 		CollectSound.Play();
+		scoreText.GetComponent<Text>().text = "1" +theScore;
 		Destroy(gameObject);
 	}
 

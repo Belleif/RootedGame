437377b1ca674e61856638787ff8a3320e7e6768
ZacharyHall95 <zhallz95@gmail.com>
2021-12-08 03:15:16 -0600

    Collectable update
    Sound now plays when collectable is collected

--- Assets/Scripts/ScoringSystem.cs
+++ Assets/Scripts/ScoringSystem.cs
@@ -5,18 +5,13 @@ using UnityEngine.UI;
 
 public class ScoringSystem : MonoBehaviour {
 
-    public GameObject scoreText;
-	public static int theScore = 0;
 	public AudioSource CollectSound;
 
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Player")
 		{
-			CollectSound.Play();
-			theScore++;
-			scoreText.GetComponent<Text>().text = theScore.ToString();
-			Debug.Log(theScore);
+            CollectSound.Play();
 			Destroy(gameObject);
 		}
 	}
